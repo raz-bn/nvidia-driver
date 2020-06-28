9de114509d95143a9d73f24b9ee948d23c405870
Raz Ben Netanel <52315689+raz-bn@users.noreply.github.com>
2020-06-28 16:54:14 +0300

    change build cri

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ KERNEL_VERSION=4.18.0-147.8.1.el8_1.x86_64
 DRIVER_VERSION=440.64.00
 RHCOS_VERSION=4.4
 
-sudo podman build -t quay.io/danclark/nvidia-driver:${DRIVER_VERSION}-1.0.0-custom-rhcos${RHCOS_VERSION} -f Dockerfile .
+sudo docker build -t quay.io/danclark/nvidia-driver:${DRIVER_VERSION}-1.0.0-custom-rhcos${RHCOS_VERSION} -f Dockerfile .
 
 exit 0
